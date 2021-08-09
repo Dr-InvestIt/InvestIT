58b364bff1820754e4400bb89dbb93f536b07986
Guangzhong Chen <guangzhong0@gmail.com>
2021-08-09 18:02:20 +0100

    Added KDJ and Combined Strat

--- Combined Strategy.py
+++ Combined Strategy.py
@@ -8,9 +8,14 @@ import yfinance as yf
 BTVERSION = tuple(int(x) for x in bt.__version__.split('.'))
 
 
-class MacdCross(bt.Strategy):
+class CombinedStrategy(bt.Strategy):
+
     params = (
-        # MACD Parameters
+        # KDJ
+        ('kdj_period', 5),
+        ('kdj_period_dfast', 3),
+        ('kdj_period_dslow', 3),
+        # MACD
         ('macd1', 12),
         ('macd2', 26),
         ('macdsig', 9),
@@ -21,56 +26,63 @@ class MacdCross(bt.Strategy):
     )
 
     def __init__(self):
-        # Main function to buy and sell
+        # KDJ Plot
+        self.kd = bt.indicators.StochasticFull(
+            self.data0, period=self.p.kdj_period, period_dfast=self.p.kdj_period_dfast, period_dslow=self.p.kdj_period_dslow)
+
+        self.K = self.kd.percD
+        self.D = self.kd.percDSlow
+        self.J = self.K*3 - self.D*2
+
+        self.kcrossover = bt.indicators.CrossOver(
+            self.K, self.D, plotname="K Cross D")
+
+        # MACD Plot
         self.macd = bt.ind.MACDHisto(
             self.data,
             period_me1=self.p.macd1,
             period_me2=self.p.macd2,
             period_signal=self.p.macdsig)
 
-        self.mcross = bt.ind.CrossOver(self.macd.macd, self.macd.signal)
+        self.mcross = bt.ind.CrossOver(
+            self.macd.macd, self.macd.signal, plotname="MACD Cross")
+
+        self.signalcross = bt.ind.CrossOver(
+            self.macd, bt.LineNum(0.0), plotname="Signal Cross 0")
 
         self.atr = bt.ind.ATR(
             self.data, period=self.p.atrperiod, plotname='ATR')
 
         self.sma = bt.ind.SMA(
-            self.data, period=self.p.smaperiod, plotname='SMA')
+            self.data, period=self.p.smaperiod, plotname='SMA', plot=False)
 
         self.smadir = self.sma - self.sma(-self.p.dirperiod)
 
     def start(self):
-        self.order = None  # to avoid operations on pending order
+        self.order = None
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            return
+
+        if order.status in [order.Completed, order.Canceled, order.Margin, order.Rejected]:
+            self.order = None
 
     def next(self):
         if self.order:
-            return  # pending order execution
+            return
 
-        if not self.position:  # not in the market
-            if self.mcross[0] > 0.0 and self.smadir < 0.0:  # logic for deciding when to buy
+        if not self.position:
+            if self.signalcross[0] == -1 and self.kd[0] <= 20:
                 self.order = self.buy()
-                pdist = self.atr[0] * self.p.atrdist
-                self.pstop = self.data.close[0] - pdist
 
-        else:  # in the market
-            pclose = self.data.close[0]
-            pstop = self.pstop
-
-            if pclose < pstop:
-                self.close()  # stop met - get out
-            else:
-                pdist = self.atr[0] * self.p.atrdist
-                self.pstop = max(pstop, pclose - pdist)
-
-    def notify_order(self, order):
-        if order.status == order.Completed:
-            pass
-
-        if not order.alive():
-            self.order = None
+        else:
+            if self.signalcross[0] == 1 and self.kd[0] >= 80:
+                self.order = self.sell()
 
 
 cerebro = bt.Cerebro()
-cerebro.addstrategy(MacdCross)
+cerebro.addstrategy(CombinedStrategy)
 
 # Define your backtest
 stock = 'AAPL'
@@ -84,28 +96,7 @@ data = bt.feeds.PandasData(dataname=yf.download(
 
 cerebro.adddata(data)
 
-
-class FixedPerc(bt.Sizer):
-    '''This sizer simply returns a fixed size for any operation
-
-    Params:
-      - ``perc`` (default: ``0.20``) Perc of cash to allocate for operation
-    '''
-
-    params = (
-        ('perc', 0.20),  # perc of cash to use for operation
-    )
-
-    def _getsizing(self, comminfo, cash, data, isbuy):
-        cashtouse = self.p.perc * cash
-        if BTVERSION > (1, 7, 1, 93):
-            size = comminfo.getsize(data.close[0], cashtouse)
-        else:
-            size = cashtouse // data.close[0]
-        return size
-
-
-cerebro.addsizer(FixedPerc, perc=0.9)
+cerebro.addsizer(bt.sizers.AllInSizer, percents=99)
 
 # Add TimeReturn Analyzers for self and the benchmark data
 cerebro.addanalyzer(bt.analyzers.TimeReturn, _name='alltime_roi',
---
+++ KDJ.py
@@ -0,0 +1,104 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+from datetime import datetime
+import backtrader as bt
+import yfinance as yf
+
+BTVERSION = tuple(int(x) for x in bt.__version__.split('.'))
+
+
+class KDJStrategy(bt.Strategy):
+    params = (
+        ('period', 9),
+        ('period_dfast', 3),
+        ('period_dslow', 3),
+    )
+
+    def __init__(self):
+
+        self.kd = bt.indicators.StochasticFull(
+            self.data0, period=self.p.period, period_dfast=self.p.period_dfast, period_dslow=self.p.period_dslow)
+
+        self.K = self.kd.percD
+        self.D = self.kd.percDSlow
+        self.J = self.K*3 - self.D*2
+
+        self.crossover = bt.indicators.CrossOver(self.K, self.D, plot=False)
+        # self.above
+
+        self.buy_signal = (self.crossover == 1)
+        self.sell_signal = (self.crossover == -1)
+
+        self.order = None
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            return
+
+        if order.status in [order.Completed, order.Canceled, order.Margin, order.Rejected]:
+            self.order = None
+
+    def next(self):
+        if self.order:
+            return
+
+        if not self.position:
+            if self.buy_signal[0]:
+                self.order = self.buy()
+
+        else:
+            if self.sell_signal[0]:
+                self.order = self.sell()
+
+
+cerebro = bt.Cerebro()
+cerebro.addstrategy(KDJStrategy)
+
+# Define your backtest
+stock = 'AAPL'
+
+fromdate = '2015-07-06'
+
+todate = '2020-01-01'
+
+data = bt.feeds.PandasData(dataname=yf.download(
+    stock, fromdate, todate, auto_adjust=True))
+
+cerebro.adddata(data)
+
+cerebro.addsizer(bt.sizers.AllInSizer, percents=99)
+
+# Add TimeReturn Analyzers for self and the benchmark data
+cerebro.addanalyzer(bt.analyzers.TimeReturn, _name='alltime_roi',
+                    timeframe=bt.TimeFrame.NoTimeFrame)
+
+cerebro.addanalyzer(bt.analyzers.TimeReturn, data=data, _name='benchmark',
+                    timeframe=bt.TimeFrame.NoTimeFrame)
+
+# Add TimeReturn Analyzers fot the annuyl returns
+cerebro.addanalyzer(bt.analyzers.TimeReturn, timeframe=bt.TimeFrame.Years)
+# Add a SharpeRatio
+cerebro.addanalyzer(bt.analyzers.SharpeRatio, timeframe=bt.TimeFrame.Years,
+                    riskfreerate=0.01)
+
+# Add SQN to qualify the trades
+cerebro.addanalyzer(bt.analyzers.SQN)
+cerebro.addobserver(bt.observers.DrawDown)  # visualize the drawdown evol
+
+cerebro.broker.setcash(10000.00)
+
+print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+start_portfolio_value = cerebro.broker.getvalue()
+
+results = cerebro.run()
+result = results[0]
+
+print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+end_portfolio_value = cerebro.broker.getvalue()
+print("PnL:", end_portfolio_value - start_portfolio_value)
+
+for alyzer in result.analyzers:
+    alyzer.print()
+
+cerebro.plot()
